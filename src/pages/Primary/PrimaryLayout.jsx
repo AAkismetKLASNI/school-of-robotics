9b1a4b32f011d1321ff662ab8a2311b7abc5d3af
NeVzaimnoePadenie <118021544+AAkismetKLASNI@users.noreply.github.com>
2024-04-20 11:25:04 +0500

    фикс с изображениями

--- src/pages/Primary/PrimaryLayout.jsx
+++ src/pages/Primary/PrimaryLayout.jsx
@@ -10,7 +10,23 @@ export const PrimaryLayout = () => {
 				</H>
 				<p>Школа программирования и робототехники для детей и подростков</p>
 				<Button btnBlue2>ПОПРОБОВАТЬ БЕСПЛАТНО</Button>
-				<div></div>
+				<div>
+					<img
+						src="https://i.postimg.cc/mZSfQ0W8/Group-6.png"
+						border="0"
+						alt="Group-6"
+					/>
+					<img
+						src="https://i.postimg.cc/FHZSyFsD/inst.png"
+						border="0"
+						alt="inst"
+					/>
+					<img
+						src="https://i.postimg.cc/DzDy3Rwq/Group-7.png"
+						border="0"
+						alt="Group-7"
+					/>
+				</div>
 			</div>
 		</div>
 	);
